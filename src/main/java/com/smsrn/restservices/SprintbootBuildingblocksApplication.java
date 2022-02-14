4e43660b1de3b2d1342c701959f6e7a9418a0f8f
sibtain.raza <sibtain.raza@10pearls.com>
2022-02-15 00:02:43 +0500

    First Commit

---
+++ src/main/java/com/smsrn/restservices/SprintbootBuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.smsrn.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootBuildingblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smsrn/restservices/SprintbootBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smsrn.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
