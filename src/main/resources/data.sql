56c61659bd8952260a5af6be9f561884ed243ea8
sibtain.raza <sibtain.raza@10pearls.com>
2022-02-16 02:38:00 +0500

    JPA Base RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(101,'smsibtainrn101@gmail.com','Sibtain101','Raza101','admin','ssn101','smsibtain101');
+insert into user values(102,'smsibtainrn102@gmail.com','Sibtain102','Raza102','admin','ssn102','smsibtain102');
+insert into user values(103,'smsibtainrn103@gmail.com','Sibtain103','Raza103','admin','ssn103','smsibtain103');
+insert into user values(104,'smsibtainrn104@gmail.com','Sibtain104','Raza104','admin','ssn104','smsibtain104');
+insert into user values(105,'smsibtainrn105@gmail.com','Sibtain105','Raza105','admin','ssn105','smsibtain105');
