968792f358f70417e3f845272e7b37499c0f0e11
sibtain.raza <sibtain.raza@10pearls.com>
2022-02-15 15:54:45 +0500

    Second Commit

--- src/main/java/com/smsrn/restservices/SprintbootBuildingblocksApplication.java
+++ src/main/java/com/smsrn/restservices/SprintbootBuildingblocksApplication.java
@@ -3,6 +3,8 @@ package com.smsrn.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// SMSRN
+
 @SpringBootApplication
 public class SprintbootBuildingblocksApplication {
 
