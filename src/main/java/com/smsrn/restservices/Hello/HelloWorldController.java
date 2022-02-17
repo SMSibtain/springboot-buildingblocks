4fa7a1d35dca5fd349a7019ed7b4b02b1be1ee90
sibtain.raza <sibtain.raza@10pearls.com>
2022-02-17 17:45:10 +0500

    i18n - First Commit

--- src/main/java/com/smsrn/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/smsrn/restservices/Hello/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.smsrn.restservices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 
 	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
 //	@GetMapping("/helloworld")
@@ -17,5 +26,15 @@ public class HelloWorldController {
 	@GetMapping("/helloworld-bean")
 	public UserDetails helloWorldBean() {
 		return new UserDetails("Sibtain", "Raza", "Karachi");
+	}
+
+	@GetMapping("/hello-int")
+	public String getMessagesI18NFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+	
+	@GetMapping("/hello-int2")
+	public String getMessagesI18NFormat() {
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
 	}
 }
--- src/main/java/com/smsrn/restservices/SprintbootBuildingblocksApplication.java
+++ src/main/java/com/smsrn/restservices/SprintbootBuildingblocksApplication.java
@@ -1,7 +1,13 @@
 package com.smsrn.restservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 // SMSRN
 
@@ -12,4 +18,17 @@ public class SprintbootBuildingblocksApplication {
 		SpringApplication.run(SprintbootBuildingblocksApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
