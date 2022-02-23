4646273cc481b548e33dc8562e88d8b52f221e4c
sibtain.raza <sibtain.raza@10pearls.com>
2022-02-23 15:00:53 +0500

    Swagger Config - First Commit

---
+++ src/main/java/com/smsrn/restservices/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.smsrn.restservices.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(getApiInfo()).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
+
+	// Swagger Metadata : http://localhost:8081/v2/api-docs
+	// Swagger UI URL : http://localhost:8081/swagger-ui.html
+
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("SMSRN User Management Service")
+				.description("This page list all apis of user management").version("2.0")
+				.contact(new Contact("Sibtain", "", "smsibtainrn@gmail.com")).license("License 2.0").licenseUrl("")
+				.build();
+	}
+}
